7237ac08fe6489c0275fdc104886bda27801cef0
Nelson Foltz <nelsfoltz@gmail.com>
2020-12-28 10:45:57 -0500

    add: formspree setup

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -14,11 +14,16 @@ const Contact = () => {
             shorts stumptown locavore cray tofu beard post-ironic readymade
             hoodie glossier scenester.
           </p>
-          <form className="contact-form">
+          <form
+            className="contact-form"
+            action="https://formspree.io/f/xnqoooan"
+            method="POST"
+          >
             <input
               type="email"
               className="form-input"
               placeholder="enter email"
+              name="_replyto"
             />
             <button type="submit" className="submit-btn">
               subscribe
