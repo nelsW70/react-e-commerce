9a85c01397dcc907590373a9a2dd4d65f91c7e28
Nelson Foltz <nelsfoltz@gmail.com>
2020-12-30 13:57:26 -0500

    add: filters - colors

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -64,7 +64,6 @@ const Filters = () => {
           </div>
           {/* end of categories */}
           {/* companies */}
-          {/* end  of companies */}
           <div className="form-control">
             <h5>company</h5>
             <select
@@ -82,6 +81,45 @@ const Filters = () => {
               })}
             </select>
           </div>
+          {/* end  of companies */}
+          {/* colors */}
+          <div className="form-control">
+            <h5>colors</h5>
+            <div className="colors">
+              {colors.map((col, index) => {
+                if (col === 'all') {
+                  return (
+                    <button
+                      key={index}
+                      name="color"
+                      onClick={updateFilters}
+                      data-color="all"
+                      className={`${
+                        color === 'all' ? 'all-btn active' : 'all-btn'
+                      }`}
+                    >
+                      all
+                    </button>
+                  );
+                }
+                return (
+                  <button
+                    key={index}
+                    name="color"
+                    style={{ background: col }}
+                    className={`${
+                      color === col ? 'color-btn active' : 'color-btn'
+                    }`}
+                    data-color={col}
+                    onClick={updateFilters}
+                  >
+                    {color === col ? <FaCheck /> : null}
+                  </button>
+                );
+              })}
+            </div>
+          </div>
+          {/* end of colors */}
         </form>
       </div>
     </Wrapper>
