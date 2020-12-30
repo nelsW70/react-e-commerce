1660176571f55e0f6b67f161a176ec53dc89d127
Nelson Foltz <nelsfoltz@gmail.com>
2020-12-30 10:05:49 -0500

    add: sort functionality

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -29,6 +29,10 @@ export const FilterProvider = ({ children }) => {
     dispatch({ type: LOAD_PRODUCTS, payload: products });
   }, [products]);
 
+  useEffect(() => {
+    dispatch({ type: SORT_PRODUCTS });
+  }, [products, state.sort]);
+
   const setGridView = () => {
     dispatch({ type: SET_GRIDVIEW });
   };
@@ -36,7 +40,6 @@ export const FilterProvider = ({ children }) => {
     dispatch({ type: SET_LISTVIEW });
   };
   const updateSort = e => {
-    // const name = e.target.name;
     const value = e.target.value;
     dispatch({ type: UPDATE_SORT, payload: value });
   };
